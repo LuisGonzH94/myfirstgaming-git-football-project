0d6b0c84965be21ed3b8d9d11e984b662abcde4d
LuisGonzH94 <you@example.com>
2023-03-11 18:39:11 -0800

    Added an overview playfield for player scope.

---
+++ football.py
@@ -0,0 +1,18 @@
+player_one_name = ""
+player_two_name = ""
+
+info_game_class = ["setup", "player", "tokens", "coordinates"]
+
+# letter 'x' will represent miss and letter 'o' will represent hit on token, and big 'G' will represent 'GOAL'!
+#if 'o', then the player will either choose to find the goal by guessing from A to B on axis zero.
+#Or player can choose to find another token. The faster the player find tokens, the higher chance to find the goal. 
+
+print("  A | B | C | D | E | F")
+#I will work on creating a random choice to hide the goal from A-B on axis zero.
+print("0|__________________|__|")
+print("1|__|___|___|___|___|__|")
+print("2|__|___|___|___|___|__|")
+print("3|__|___|___|___|___|__|")
+print("4|__|___|___|___|___|__|")
+print("5|__|___|___|___|___|__|")
+print("6|__|___|___|___|___|__|")
