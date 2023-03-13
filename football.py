07e884a5aee54d74845a776843a4409a14ce1fd2
LuisGonzH94 <you@example.com>
2023-03-13 14:51:04 -0700

    Started over to make changes the right way.

--- football.py
+++ football.py
@@ -1,27 +1,38 @@
+#Started all over again because I did not want to copy and paste from other coders that create the battleship game. 
+#Although, I am doing some research since there are some functions I need to keep practicing. 
+#However, this file is not a battleship game; it is a football game where the user will need to find the hidden players('O') and score. 
+#Rules: a) Find the 5 players. b) If one player is found, then the user can shoot to goal. Else, user can try finding another player until getting 5.
+#c) Turns will vary depending on how fast the user find the hidden players. One player found, one extra turn to shoot to goal.
+#d) Last, The higher goals, the better chance to beat the game. Choices will be on for user to decide. Get goals, or find all players first. 
+
 import random
+#Created a board 1x6 that goes through A-F
+print(" |A|B|C|D|E|F|")
 
-# letter 'x' will represent miss and letter 'o' will represent hit on token, and big 'G' will represent 'GOAL'!
-#if 'o', then the player will either choose to find the goal by guessing from A to B on axis zero.
-#Or player can choose to find another token. The faster the player find tokens, the higher chance to find the goal. 
+for x in range(1, 7):
+    row = ""
+    for y in range(6):
+        col = "_|"
+        row = row + col
+    print(x, row)
 
-hidden_dots = [[" "]*7 for x in range(7)]
-visible_dots = [[" "]*6 for x in range(6)]
+#Goal setup, using random.choice
+goal_t = "T"
+goal_u = "Y"
+ran_goal = random.choice([goal_t, goal_u])
 
-# 'G' for GOAL and 'M' for Miss(Redo)
-#random.choice will be used to display goal as a 'G' or 'M'
-g_m_goals = [["G"], ["M"]]
-ran_c = random.choice(g_m_goals)
+shoot = "Y"
+for goal in ran_goal:
+    r1 = ran_goal 
+    print("7","---", r1, "-----")   
+points = 0
+if shoot == r1:
+    print("GOAL!, You score 1 point")
+    points += 1
+else:
+    print("It's a miss")
+    points -= 0
+    
+print("Your current points: ", points) 
+    
 
-letter_to_num = {" ": 0, "A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6}
-
-#Created a function to display the football board.
-#Noted %d and %s, which is for number and string. 
-def display_board(board):
-    print("   A| B | C | D | E | F")
-    print("       ", ran_c)
-    row_num = 1
-    for row in board:
-        print("%d|%s" %(row_num, "_|_".join(row)))
-        row_num += 1
-    return "Keep moving forward"
-print(display_board(visible_dots))
