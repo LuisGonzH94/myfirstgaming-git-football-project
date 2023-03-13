a80b4f3a47a59479c447864e50fb72419e5191df
LuisGonzH94 <you@example.com>
2023-03-12 17:59:16 -0700

    Created a function to display football board

--- football.py
+++ football.py
@@ -1,18 +1,27 @@
-player_one_name = ""
-player_two_name = ""
-
-info_game_class = ["setup", "player", "tokens", "coordinates"]
+import random
 
 # letter 'x' will represent miss and letter 'o' will represent hit on token, and big 'G' will represent 'GOAL'!
 #if 'o', then the player will either choose to find the goal by guessing from A to B on axis zero.
 #Or player can choose to find another token. The faster the player find tokens, the higher chance to find the goal. 
 
-print("  A | B | C | D | E | F")
-#I will work on creating a random choice to hide the goal from A-B on axis zero.
-print("0|__________________|__|")
-print("1|__|___|___|___|___|__|")
-print("2|__|___|___|___|___|__|")
-print("3|__|___|___|___|___|__|")
-print("4|__|___|___|___|___|__|")
-print("5|__|___|___|___|___|__|")
-print("6|__|___|___|___|___|__|")
+hidden_dots = [[" "]*7 for x in range(7)]
+visible_dots = [[" "]*6 for x in range(6)]
+
+# 'G' for GOAL and 'M' for Miss(Redo)
+#random.choice will be used to display goal as a 'G' or 'M'
+g_m_goals = [["G"], ["M"]]
+ran_c = random.choice(g_m_goals)
+
+letter_to_num = {" ": 0, "A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6}
+
+#Created a function to display the football board.
+#Noted %d and %s, which is for number and string. 
+def display_board(board):
+    print("   A| B | C | D | E | F")
+    print("       ", ran_c)
+    row_num = 1
+    for row in board:
+        print("%d|%s" %(row_num, "_|_".join(row)))
+        row_num += 1
+    return "Keep moving forward"
+print(display_board(visible_dots))
